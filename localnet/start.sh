2e17698d498f49035d7e04c1ead0cf97cc63153c
Adrian Sutton <adrian.sutton@consensys.net>
2021-10-08 11:03:13 +1000

    Improve layout

--- localnet/start.sh
+++ localnet/start.sh
@@ -26,22 +26,22 @@ CONSENSUS_BOOTNODE="enr:-KG4QNndG4nlf0_K6G2NOQ_ifmraOlseY7ZbsDQ0NWk2pmxjE-bi6SQT
 EXECUTION_BOOTNODE="enode://3a514176466fa815ed481ffad09110a2d344f6c9b78c1d14afc351c3a51be33d8072e77939dc03ba44790779b7a1025baf3003f6732430e20cd9b76d953391b3@127.0.0.1:30308"
 
 echo "Generating consensus genesis state..."
-eth2-testnet-genesis phase0 \
-  --config consensus/phase0.yaml \
-  --eth1-block 0xd20a4f7d29f3a524f893485aab52fad41ebcfc7d2095fc704d677417b9169919 \
-  --timestamp $(date +%s) \
-  --mnemonics consensus/mnemonics.yaml \
-  --state-output "${GENESIS_STATE}" \
-  --tranches-dir "${SCRATCH}/localnet/validator-pubkeys"
+#eth2-testnet-genesis phase0 \
+#  --config consensus/phase0.yaml \
+#  --eth1-block 0xd20a4f7d29f3a524f893485aab52fad41ebcfc7d2095fc704d677417b9169919 \
+#  --timestamp $(date +%s) \
+#  --mnemonics consensus/mnemonics.yaml \
+#  --state-output "${GENESIS_STATE}" \
+#  --tranches-dir "${SCRATCH}/localnet/validator-pubkeys"
 
 # Create consensus testnet dir
 BEACONSPEC_DIR="${SCRATCH}/localnet/beaconspec"
-mkdir -p "${BEACONSPEC_DIR}"
-cp "${GENESIS_STATE}" "${BEACONSPEC_DIR}/genesis.ssz"
-echo "[]" > "${BEACONSPEC_DIR}/boot_enr.yaml"
-echo "$CONSENSUS_BOOTNODE" > "${BEACONSPEC_DIR}/boostrap_nodes.txt"
-cp consensus/config.yaml "${BEACONSPEC_DIR}/config.yaml"
-echo "0" > "${BEACONSPEC_DIR}/deploy_block.txt"
+#mkdir -p "${BEACONSPEC_DIR}"
+#cp "${GENESIS_STATE}" "${BEACONSPEC_DIR}/genesis.ssz"
+#echo "[]" > "${BEACONSPEC_DIR}/boot_enr.yaml"
+#echo "$CONSENSUS_BOOTNODE" > "${BEACONSPEC_DIR}/boostrap_nodes.txt"
+#cp consensus/config.yaml "${BEACONSPEC_DIR}/config.yaml"
+#echo "0" > "${BEACONSPEC_DIR}/deploy_block.txt"
 
 # Make sure keys aren't locked from previous runs. Terrible for slashing but oh well...
 find consensus/validator-keys -name '*.lock' -delete
@@ -50,127 +50,190 @@ tmux kill-session -t merge-localnet || true
 
 
 echo "### Node 1 - Geth + Teku"
-$GETH --datadir "${SCRATCH}/localnet/data/geth1" init execution/genesis.json
-$GETH --datadir "${SCRATCH}/localnet/data/geth1" account import --password "execution/geth/passfile.txt" execution/signer.key
-#  sh -c "$BESU --config-file execution/besu/config.toml --data-path \"${SCRATCH}/localnet/data/besu1\"  --p2p-port 30308 --rpc-http-port=8545 --node-private-key-file=execution/signer.key | tee \"${SCRATCH}/localnet/data/besu1/besu.log\"" \
+#$GETH --datadir "${SCRATCH}/localnet/data/geth1" init execution/genesis.json
+#$GETH --datadir "${SCRATCH}/localnet/data/geth1" account import --password "execution/geth/passfile.txt" execution/signer.key
+cat <<EOF > "${SCRATCH}/localnet/startBesu1.sh"
+  $BESU \
+      --config-file execution/besu/config.toml \
+      --data-path \"${SCRATCH}/localnet/data/besu1\" \
+      --p2p-port 30308 \
+      --rpc-http-port=8545 \
+      --node-private-key-file=execution/signer.key \
+      | tee \"${SCRATCH}/localnet/data/besu1/besu.log\"
+EOF
+chmod a+x "${SCRATCH}/localnet/startBesu1.sh"
+
 tmux new-session -d -s merge-localnet \
-  $GETH \
-    --catalyst \
-    --http \
-    --http.api "engine,eth" \
-    --http.port 8545 \
-    --allow-insecure-unlock \
-    --unlock "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b" \
-    --password "execution/geth/passfile.txt" \
-    --datadir "${SCRATCH}/localnet/data/geth1" \
-    --nodekey "execution/signer.key" \
-    --bootnodes "$EXECUTION_BOOTNODE" \
-    --port 30308 \
-    --syncmode full \
-    --mine \
-    --networkid 2337002 \
-    console \
-    \; set-option remain-on-exit on
+  ./show "Geku (Geth)" \
+  \; set-option remain-on-exit on
+#  $GETH \
+#    --catalyst \
+#    --http \
+#    --http.api "engine,eth" \
+#    --http.port 8545 \
+#    --allow-insecure-unlock \
+#    --unlock "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b" \
+#    --password "execution/geth/passfile.txt" \
+#    --datadir "${SCRATCH}/localnet/data/geth1" \
+#    --nodekey "execution/signer.key" \
+#    --bootnodes "$EXECUTION_BOOTNODE" \
+#    --port 30308 \
+#    --syncmode full \
+#    --mine \
+#    --networkid 2337002 \
+#    console \
+#    \; set-option remain-on-exit on
+
+tmux set-hook -g after-split-window "selectl even-vertical"
 
 tmux split-window -h -t merge-localnet \
-  $TEKU \
-    --eth1-endpoints http://127.0.0.1:8545 \
-    --ee-fee-recipient-address=0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 \
-    --validator-keys "consensus/validator-keys/batch1/teku-keys:consensus/validator-keys/batch1/teku-secrets" \
-    --validator-keys "consensus/validator-keys/batch2/teku-keys:consensus/validator-keys/batch2/teku-secrets" \
-    --validators-keystore-locking-enabled=false \
-    --network=consensus/config.yaml \
-    --initial-state "${GENESIS_STATE}" \
-    --Xnetwork-merge-total-terminal-difficulty=${TERMINAL_TOTAL_DIFFICULTY} \
-    --p2p-private-key-file=consensus/teku/teku.key \
-    --p2p-advertised-ip=127.0.0.1 \
-    --p2p-port 9000 \
-    --Xlog-include-p2p-warnings-enabled \
-    --rest-api-enabled \
-    --data-path "${SCRATCH}/localnet/data/teku1"
+  ./show "Geku (Teku)"
+#  $TEKU \
+#    --eth1-endpoints http://127.0.0.1:8545 \
+#    --ee-fee-recipient-address=0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 \
+#    --validator-keys "consensus/validator-keys/batch1/teku-keys:consensus/validator-keys/batch1/teku-secrets" \
+#    --validators-keystore-locking-enabled=false \
+#    --network=consensus/config.yaml \
+#    --initial-state "${GENESIS_STATE}" \
+#    --Xnetwork-merge-total-terminal-difficulty=${TERMINAL_TOTAL_DIFFICULTY} \
+#    --p2p-private-key-file=consensus/teku/teku.key \
+#    --p2p-advertised-ip=127.0.0.1 \
+#    --p2p-port 9000 \
+#    --Xlog-include-p2p-warnings-enabled \
+#    --rest-api-enabled \
+#    --data-path "${SCRATCH}/localnet/data/teku1"
 
 
-echo "### Node 2 - Geth + Teku"
-$GETH --datadir "${SCRATCH}/localnet/data/geth2" init execution/genesis.json
-tmux split-window -v -t %0 \
-  $GETH \
-    --catalyst \
-    --http \
-    --http.api "engine,eth" \
-    --http.port 8546 \
-    --datadir "${SCRATCH}/localnet/data/geth2" \
-    --bootnodes "$EXECUTION_BOOTNODE" \
-    --port 30304 \
-    --syncmode full \
-    --networkid 2337002 \
-    console
+echo "### Node 2 - Besu + Teku"
 
-# Use besu instead:
-#sh -c "$BESU --config-file execution/besu/config.toml --data-path \"${SCRATCH}/localnet/data/besu2\"  --p2p-port 30309 --rpc-http-port=8546 --bootnodes \"${EXECUTION_BOOTNODE}\" | tee \"${SCRATCH}/localnet/data/besu2/besu.log\""
+cat <<EOF > "${SCRATCH}/localnet/startBesu2.sh"
+  $BESU \\
+    --config-file execution/besu/config.toml \\
+    --data-path "${SCRATCH}/localnet/data/besu2" \\
+    --p2p-port 30304 \\
+    --rpc-http-port=8546 \\
+    --bootnodes "${EXECUTION_BOOTNODE}" \\
+    | tee "${SCRATCH}/localnet/data/besu2/besu.log"
+EOF
+chmod a+x "${SCRATCH}/localnet/startBesu2.sh"
 
-tmux split-window -v -t %1 \
-  $TEKU \
-    --eth1-endpoints http://127.0.0.1:8546 \
-    --ee-fee-recipient-address=0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 \
-    --validator-keys "consensus/validator-keys/batch3/teku-keys:consensus/validator-keys/batch3/teku-secrets" \
-    --validators-keystore-locking-enabled=false \
-    --network=consensus/config.yaml \
-    --initial-state "${GENESIS_STATE}" \
-    --Xnetwork-merge-total-terminal-difficulty=${TERMINAL_TOTAL_DIFFICULTY} \
-    --p2p-advertised-ip=127.0.0.1 \
-    --p2p-port 9001 \
-    --Xlog-include-p2p-warnings-enabled \
-    --p2p-discovery-bootnodes "${CONSENSUS_BOOTNODE}" \
-    --p2p-static-peers "/ip4/127.0.0.1/tcp/9000/p2p/16Uiu2HAmFojnD68tVG9yKjHApYTvyjtnQ2nMbxJpoDNiMPGVdyGP" \
-    --rest-api-enabled \
-    --rest-api-port 5052 \
-    --data-path "${SCRATCH}/localnet/data/teku2"
+tmux split-window -v -f -t %0 ./show "Beku (Besu)" # "${SCRATCH}/localnet/startBesu2.sh"
 
+tmux split-window -h -t %2 \
+  ./show "Beku (Teku)"
+#  $TEKU \
+#    --eth1-endpoints http://127.0.0.1:8546 \
+#    --ee-fee-recipient-address=0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 \
+#    --validator-keys "consensus/validator-keys/batch2/teku-keys:consensus/validator-keys/batch2/teku-secrets" \
+#    --validators-keystore-locking-enabled=false \
+#    --network=consensus/config.yaml \
+#    --initial-state "${GENESIS_STATE}" \
+#    --Xnetwork-merge-total-terminal-difficulty=${TERMINAL_TOTAL_DIFFICULTY} \
+#    --p2p-advertised-ip=127.0.0.1 \
+#    --p2p-port 9001 \
+#    --Xlog-include-p2p-warnings-enabled \
+#    --p2p-discovery-bootnodes "${CONSENSUS_BOOTNODE}" \
+#    --p2p-static-peers "/ip4/127.0.0.1/tcp/9000/p2p/16Uiu2HAmFojnD68tVG9yKjHApYTvyjtnQ2nMbxJpoDNiMPGVdyGP" \
+#    --rest-api-enabled \
+#    --rest-api-port 5052 \
+#    --data-path "${SCRATCH}/localnet/data/teku2"
 
 echo "### Node 3 - Geth + Lighthouse"
 $GETH --datadir "${SCRATCH}/localnet/data/geth3" init execution/genesis.json
-tmux split-window -v -t %2 \
-  $GETH \
-    --catalyst \
-    --http \
-    --http.api "engine,eth" \
-    --http.port 8547 \
-    --datadir "${SCRATCH}/localnet/data/geth3" \
-    --bootnodes "$EXECUTION_BOOTNODE" \
-    --port 30305 \
-    --syncmode full \
-    --networkid 2337002 \
-    console
+tmux split-window -v -f -t %2 \
+  ./show "Gethouse (Geth)"
+#  $GETH \
+#    --catalyst \
+#    --http \
+#    --http.api "engine,eth" \
+#    --http.port 8547 \
+#    --datadir "${SCRATCH}/localnet/data/geth3" \
+#    --bootnodes "$EXECUTION_BOOTNODE" \
+#    --port 30305 \
+#    --syncmode full \
+#    --networkid 2337002 \
+#    console
 
-tmux split-window -v -t %3 \
-  $LIGHTHOUSE \
-    --spec mainnet \
-    --testnet-dir "${BEACONSPEC_DIR}" \
-    beacon_node \
-    --purge-db \
-    --datadir "${SCRATCH}/localnet/data/lighthouse1" \
-    --boot-nodes "${CONSENSUS_BOOTNODE}" \
-    --port 9002 \
-    --dummy-eth1 \
-    --http \
-    --http-port 5053 \
-    --http-allow-sync-stalled \
-    --merge \
-    --execution-endpoints http://127.0.0.1:8547 \
-    --terminal-total-difficulty-override 12C
+tmux split-window -h -t %4 \
+  ./show "Gethouse (Lighthouse BN)"
+#  $LIGHTHOUSE \
+#    --spec mainnet \
+#    --testnet-dir "${BEACONSPEC_DIR}" \
+#    beacon_node \
+#    --purge-db \
+#    --datadir "${SCRATCH}/localnet/data/lighthouse1" \
+#    --boot-nodes "${CONSENSUS_BOOTNODE}" \
+#    --port 9002 \
+#    --dummy-eth1 \
+#    --http \
+#    --http-port 5053 \
+#    --http-allow-sync-stalled \
+#    --merge \
+#    --execution-endpoints http://127.0.0.1:8547 \
+#    --terminal-total-difficulty-override 12C
 # Lighthouse Validator
 mkdir -p "${SCRATCH}/localnet/data/lighthouse1-vc"
-cp -rf consensus/validator-keys/batch4/keys "${SCRATCH}/localnet/data/lighthouse1-vc/"
-cp -rf consensus/validator-keys/batch4/secrets "${SCRATCH}/localnet/data/lighthouse1-vc/"
+cp -rf consensus/validator-keys/batch3/keys "${SCRATCH}/localnet/data/lighthouse1-vc/"
+cp -rf consensus/validator-keys/batch3/secrets "${SCRATCH}/localnet/data/lighthouse1-vc/"
 tmux split-window -v -t %5 \
-  $LIGHTHOUSE \
-    --spec mainnet \
-    --testnet-dir "${BEACONSPEC_DIR}" \
-    vc \
-    --beacon-nodes http://127.0.0.1:5053 \
-    --init-slashing-protection \
-    --validators-dir "${SCRATCH}/localnet/data/lighthouse1-vc/keys" \
-    --secrets-dir "${SCRATCH}/localnet/data/lighthouse1-vc/secrets"
+  ./show "Gethouse (Lighthouse VC)"
+#  $LIGHTHOUSE \
+#    --spec mainnet \
+#    --testnet-dir "${BEACONSPEC_DIR}" \
+#    vc \
+#    --beacon-nodes http://127.0.0.1:5053 \
+#    --init-slashing-protection \
+#    --validators-dir "${SCRATCH}/localnet/data/lighthouse1-vc/keys" \
+#    --secrets-dir "${SCRATCH}/localnet/data/lighthouse1-vc/secrets"
+
+
+echo "### Node 4 - Besu + Lighthouse"
+
+cat <<EOF > "${SCRATCH}/localnet/startBesu3.sh"
+  $BESU \\
+    --config-file execution/besu/config.toml \\
+    --data-path "${SCRATCH}/localnet/data/besu3" \\
+    --p2p-port 30306 \\
+    --rpc-http-port=8548 \\
+    --bootnodes "${EXECUTION_BOOTNODE}" \\
+    | tee "${SCRATCH}/localnet/data/besu3/besu.log"
+EOF
+chmod a+x "${SCRATCH}/localnet/startBesu3.sh"
+
+tmux split-window -v -f -t %0 ./show "Beshouse (Besu)" # "${SCRATCH}/localnet/startBesu3.sh"
+
+tmux split-window -h -t %7 \
+  ./show "Beshouse (Lighthouse BN)"
+#  $LIGHTHOUSE \
+#    --spec mainnet \
+#    --testnet-dir "${BEACONSPEC_DIR}" \
+#    beacon_node \
+#    --purge-db \
+#    --datadir "${SCRATCH}/localnet/data/lighthouse2" \
+#    --boot-nodes "${CONSENSUS_BOOTNODE}" \
+#    --port 9003 \
+#    --dummy-eth1 \
+#    --http \
+#    --http-port 5054 \
+#    --http-allow-sync-stalled \
+#    --merge \
+#    --execution-endpoints http://127.0.0.1:8548 \
+#    --terminal-total-difficulty-override 12C
+
+# Lighthouse Validator
+mkdir -p "${SCRATCH}/localnet/data/lighthouse2-vc"
+cp -rf consensus/validator-keys/batch4/keys "${SCRATCH}/localnet/data/lighthouse2-vc/"
+cp -rf consensus/validator-keys/batch4/secrets "${SCRATCH}/localnet/data/lighthouse2-vc/"
+tmux split-window -v -t %8 \
+  ./show "Beshouse (Lighthouse VC)"
+#  $LIGHTHOUSE \
+#    --spec mainnet \
+#    --testnet-dir "${BEACONSPEC_DIR}" \
+#    vc \
+#    --beacon-nodes http://127.0.0.1:5054 \
+#    --init-slashing-protection \
+#    --validators-dir "${SCRATCH}/localnet/data/lighthouse2-vc/keys" \
+#    --secrets-dir "${SCRATCH}/localnet/data/lighthouse2-vc/secrets"
 
 # Nimbus currently finds peers but isn't importing any blocks and I'm not sure why.
 #echo "### Node 4 - Geth + Nimbus"
