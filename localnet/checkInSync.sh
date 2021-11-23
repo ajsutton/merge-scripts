b49aa5fc56a32e0b787e4f2dcaadb7f428e06519
Adrian Sutton <adrian.sutton@consensys.net>
2021-11-23 18:14:59 +1000

    Handle nodes being offline

--- localnet/checkInSync.sh
+++ localnet/checkInSync.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-set -euo pipefail
-echo -n "      Teku1: "; curl -s http://localhost:5051/eth/v1/beacon/blocks/head/root  | jq -r .data.root && echo -n "      Teku2: "; curl -s http://localhost:5052/eth/v1/beacon/blocks/head/root  | jq -r .data.root && echo -n "Lighthouse1: "; curl -s http://localhost:5053/eth/v1/beacon/blocks/head/root  | jq -r .data.root && echo -n "Lighthouse1: "; curl -s http://localhost:5054/eth/v1/beacon/blocks/head/root  | jq -r .data.root
+set -uo pipefail
+echo -n "      Teku1: "; curl -s http://localhost:5051/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
+echo -n "      Teku2: "; curl -s http://localhost:5052/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
+echo -n "Lighthouse1: "; curl -s http://localhost:5053/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
+echo -n "Lighthouse1: "; curl -s http://localhost:5054/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
+
+echo
+echo -n "      Geth1: "
+curl -s -H 'Content-Type: application/json' --fail --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false]}"   http://localhost:8545 | jq -c '[.result.number, .result.hash, .result.totalDifficulty]' || echo
+echo -n "      Besu1: "
+curl -s -H 'Content-Type: application/json' --fail --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false]}"   http://localhost:8546 | jq -c '[.result.number, .result.hash, .result.totalDifficulty]' || echo
+echo -n "      Geth2: "
+curl -s -H 'Content-Type: application/json' --fail --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false]}"   http://localhost:8547 | jq -c '[.result.number, .result.hash, .result.totalDifficulty]' || echo
+echo -n "      Besu2: "
+curl -s -H 'Content-Type: application/json' --fail --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false]}"   http://localhost:8548 | jq -c '[.result.number, .result.hash, .result.totalDifficulty]' || echo
