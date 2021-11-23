bdd4e8c1f7db33bb0f578cf30bdfecbedb0aab31
Adrian Sutton <adrian.sutton@consensys.net>
2021-11-24 07:59:50 +1000

    Fix name

--- localnet/checkInSync.sh
+++ localnet/checkInSync.sh
@@ -3,7 +3,7 @@ set -uo pipefail
 echo -n "      Teku1: "; curl -s http://localhost:5051/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
 echo -n "      Teku2: "; curl -s http://localhost:5052/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
 echo -n "Lighthouse1: "; curl -s http://localhost:5053/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
-echo -n "Lighthouse1: "; curl -s http://localhost:5054/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
+echo -n "Lighthouse2: "; curl -s http://localhost:5054/eth/v1/beacon/blocks/head/root  | jq -r .data.root || echo
 
 echo
 echo -n "      Geth1: "
