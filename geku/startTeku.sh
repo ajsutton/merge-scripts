540620d68ae9680c2fbe0511ce0a1ab03d9e3586
Adrian Sutton <adrian.sutton@consensys.net>
2021-11-28 19:04:45 +1000

    Update geku config

--- geku/startTeku.sh
+++ geku/startTeku.sh
@@ -12,18 +12,16 @@ DATA_DIR="${SCRATCH}/geku-teku-data"
 rm -rf "${DATA_DIR}"
 rm -rf "${GENESIS}"
 
-$TEKU genesis mock --output-file "${GENESIS}" --network minimal --Xnetwork-altair-fork-epoch 0 --validator-count 256
+$TEKU genesis mock --output-file "${GENESIS}" --network config.yaml --validator-count 256
 $TEKU \
   --eth1-endpoints http://127.0.0.1:8545 \
-  --ee-fee-recipient-address=0xfe3b557e8fb62b89f4916b721be55ceb828dbd73 \
+  --Xee-endpoint http://127.0.0.1:8545 \
+  --Xvalidators-fee-recipient-address=0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b \
   --Xinterop-enabled=true \
   --Xinterop-number-of-validators=256 \
   --Xinterop-owned-validator-start-index=0 \
   --Xinterop-owned-validator-count=256 \
-  --network=minimal \
-  --Xnetwork-altair-fork-epoch=0 \
-  --Xnetwork-merge-fork-epoch=1 \
-  --Xnetwork-merge-total-terminal-difficulty=50 \
+  --network=config.yaml \
   --p2p-private-key-file=teku.key \
   --rest-api-enabled \
   --initial-state "${GENESIS}" \
