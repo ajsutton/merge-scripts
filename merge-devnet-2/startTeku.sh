3b3ca8cebe7251a29d0354fbdf34d2798fc90eb6
Adrian Sutton <adrian.sutton@consensys.net>
2021-12-01 09:19:40 +1000

    Fix startTeku script

--- merge-devnet-2/startTeku.sh
+++ merge-devnet-2/startTeku.sh
@@ -13,5 +13,7 @@ GENESIS_STATE="../merge-testnets/merge-devnet-2/genesis.ssz"
 FINALIZED_STATE="devnet2-state.ssz"
 # Set this to "${FINALIZED_STATE}" to use optimistic sync.
 STARTING_STATE="${GENESIS_STATE}"
-$TEKU --network ../merge-testnets/merge-devnet-2/config.yaml --data-path "${DATADIR}" --initial-state "${STARTING_STATE}" --p2p-discovery-bootnodes "enr:-Iq4QKuNB_wHmWon7hv5HntHiSsyE1a6cUTK1aT7xDSU_hNTLW3R4mowUboCsqYoh1kN9v3ZoSu_WuvW9Aw0tQ0Dxv6GAXxQ7Nv5gmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk" -Xee-endpoint http://localhost:8545
+$TEKU --network ../merge-testnets/merge-devnet-2/config.yaml --data-path "${DATADIR}" --initial-state "${STARTING_STATE}" \
+  --Xee-endpoint http://localhost:8545 \
+  --p2p-discovery-bootnodes "enr:-Iq4QKuNB_wHmWon7hv5HntHiSsyE1a6cUTK1aT7xDSU_hNTLW3R4mowUboCsqYoh1kN9v3ZoSu_WuvW9Aw0tQ0Dxv6GAXxQ7Nv5gmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk"
 
